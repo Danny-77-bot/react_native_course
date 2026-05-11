0817bb3548207626e5a72bed8b3e5f23e96c7a8b
Daniel Melaku <danielmelaku1177@gmail.com>
2026-05-11 10:16:35 +0300

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Welcome to My Portfolio</Text>
-      <Text style={styles.body}>Explore my journey as a developer.</Text>
+
+      <View style={styles.card}>
+
+        <Image
+          source={{
+            uri: "https://i.pravatar.cc/300",
+          }}
+          style={styles.image}
+        />
+
+        <Text style={styles.name}>
+          Daniel Melaku
+        </Text>
+
+        <Text style={styles.job}>
+          MERN & React Native Developer
+        </Text>
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  header: { fontSize: 24, fontWeight: 'bold' },
-  body: { fontSize: 16, marginTop: 10 },
+
+  container: {
+    flex: 1,
+    backgroundColor: "#ddd",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  card: {
+    width: 300,
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 20,
+    alignItems: "center",
+  },
+
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+
+  name: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 15,
+  },
+
+  job: {
+    fontSize: 16,
+    color: "gray",
+    marginTop: 5,
+  },
+
 });
